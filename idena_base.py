2b18a0847ec5bb27f66177fb421ab814116f8195
weiliali <46802173+weiliali@users.noreply.github.com>
2023-08-13 08:34:34 +0800

    Add files via upload

--- idena_base.py
+++ idena_base.py
@@ -189,7 +189,7 @@ def img_namesearch():
             },
             {
                 "Name": "tag-value",
-                "Values": [ "img" ]
+                "Values": [ "idena" ]
             }
         ]
         }
@@ -498,3 +498,85 @@ def ture_address():
         if not all(cell.value is None for cell in row):
             row_count += 1
     return row_count
+def img_ipsearch():
+    try:
+        # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+        # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+        # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+        cred = credential.Credential(secretId, secretKey)
+        # 实例化一个http选项，可选的，没有特殊需求可以跳过
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "cvm.tencentcloudapi.com"
+
+        # 实例化一个client选项，可选的，没有特殊需求可以跳过
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        # 实例化要请求产品的client对象,clientProfile是可选的
+        client = cvm_client.CvmClient(cred, "ap-guangzhou", clientProfile)
+
+        # 实例化一个请求对象,每个接口都会对应一个request对象
+        req = models.DescribeInstancesRequest()
+        params = {
+            "Filters": [
+                {
+                    "Name": "tag-key",
+                    "Values": ["idena"]
+                },
+                {
+                    "Name": "tag-value",
+                    "Values": ["img"]
+                }
+            ]
+        }
+        req.from_json_string(json.dumps(params))
+
+        # 返回的resp是一个DescribeInstancesResponse的实例，与请求对象对应
+        resp = client.DescribeInstances(req)
+
+        a = json.loads(resp.to_json_string())
+        b = a['InstanceSet']
+        c = int(len(b) - 1)
+        while c >= 0:
+            print(b[c]['InstanceId'], b[c]['CreatedTime'], b[c]['PublicIpAddresses'])
+            c -= 1
+        print()
+    except TencentCloudSDKException as err:
+        print(err)
+def img_nodestart():
+    img=input("请输入服务器名称————ins开头")
+    import json
+    from tencentcloud.common import credential
+    from tencentcloud.common.profile.client_profile import ClientProfile
+    from tencentcloud.common.profile.http_profile import HttpProfile
+    from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+    from tencentcloud.tat.v20201028 import tat_client, models
+    try:
+        # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+        # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+        # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+        cred = credential.Credential(secretId, secretKey)
+        # 实例化一个http选项，可选的，没有特殊需求可以跳过
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "tat.tencentcloudapi.com"
+
+        # 实例化一个client选项，可选的，没有特殊需求可以跳过
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        # 实例化要请求产品的client对象,clientProfile是可选的
+        client = tat_client.TatClient(cred, "ap-guangzhou", clientProfile)
+
+        # 实例化一个请求对象,每个接口都会对应一个request对象
+        req = models.InvokeCommandRequest()
+        params = {
+            "CommandId": "cmd-p5wli3u3",
+            "InstanceIds": [img]
+        }
+        req.from_json_string(json.dumps(params))
+
+        # 返回的resp是一个InvokeCommandResponse的实例，与请求对象对应
+        resp = client.InvokeCommand(req)
+        # 输出json格式的字符串回包
+        print(resp.to_json_string())
+
+    except TencentCloudSDKException as err:
+        print(err)
--- idena_img.py
+++ idena_img.py
@@ -4,8 +4,12 @@ print("现在时间是",pro_base.today_time())
 print("选择您的操作")
 print("1.创建镜像服务器")
 print("2.创建镜像")
+print("3.查询镜像服务器并打开node")
 i=int(input())
 if i==1:
     idena_base.img_vps()
 elif i==2:
     idena_base.img_build()
+elif i==3:
+    idena_base.img_ipsearch()
+    idena_base.img_nodestart()
